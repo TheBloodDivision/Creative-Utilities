53b98b847b1159fb1f090054aea91282d4c7dfb0
TheBloodDivision <theblooddivision@gmail.com>
2015-01-25 00:19:08 +0100

    Reference setup

---
+++ src/main/java/com/TBD/CreativeUtilities/reference/Reference.java
@@ -0,0 +1,9 @@
+package com.TBD.CreativeUtilities.reference;
+
+public class Reference {
+    public static final String MOD_ID = "CreativeUtilities";
+    public static final String MOD_NAME = "Creative Utilities";
+    public static final String VERSION = "1.7.10-0.5";
+    public static final String PROXY_CLIENT = "com.TBD.CreativeUtilities.proxy.ClientProxy";
+    public static final String PROXY_SERVER = "com.TBD.CreativeUtilities.proxy.ServerProxy";
+}
